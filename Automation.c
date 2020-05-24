dec7b4b02f98cb44081212d913f3595b000987be
snitin08 <nitinpandit.2000@gmail.com>
2020-05-24 13:59:38 +0530

    Door opening

--- Automation.c
+++ Automation.c
@@ -1,10 +1,29 @@
 #include<lpc214x.h>
-#define WATER (IO0PIN & 1<<14)
+#define DOOR_OPEN (IO0PIN & 1<<14)
 #define PLOCK 0x00000400
 unsigned int adc(int no,int ch);
 void plantWatering();
 void SystemInit(void);
 void delay_ms(unsigned int j);
+void doorOpening();
+void beep();
+void beep()
+{
+	PINSEL1 |= 0x00080000; /* P0.25 as DAC output :option 3 - 10 (bits18,19)*/ 
+
+ //P0.25 used as AOUT - DAC output 
+ //val - 0 to 1023  : 10 bit DAC, P0.25 used as AOUT generates analog output 
+ unsigned int val=0;
+	for(int i=0;i<10;i++)
+	{
+	 DACR = ( (1<<16) | (1023<<6) ); 
+	 delay_ms(1); 
+		DACR = ( (1<<16) | (0<<6) ); //stop the buzzer
+		delay_ms(1); 
+	}
+ 
+ 
+}
 void delay_ms(unsigned int j)
 { 
   unsigned int x,i; 
@@ -37,7 +56,7 @@ void plantWatering()
 		 unsigned int  no_of_steps_clk = 50, no_of_steps_aclk = 50; 
 		 IO0DIR |= 1U << 31 | 0x00FF0000 | 1U << 30; // to set P0.16 to P0.23 as o/ps  
 		 
-		 SystemInit( ); 
+		 
 		do{ 
 		IO0CLR = 0X000F0000;IO0SET = 0X00010000;delay_ms(10);if(--no_of_steps_clk == 0) break; 
 		IO0CLR = 0X000F0000;IO0SET = 0X00020000;delay_ms(10);if(--no_of_steps_clk == 0) break; 
@@ -52,16 +71,16 @@ void plantWatering()
 		IO0CLR = 0X000F0000;IO0SET = 0X00010000;delay_ms(10);if(--no_of_steps_aclk == 0) break; 
 		}while(1); 
 		IO0CLR = 0X00FF0000; 
-		while(1); 
+		
 
 	}
 }
 
 unsigned int adc(int no,int ch) 
 { 
-            // adc(1,4) for humidity sensor sensor LM34, digital value will increase as temp increases 
- // adc(1,3) for LDR - digival value will reduce as the light increases 
- // adc(1,2) for trimpot - digital value changes as the pot rotation 
+            // adc(1,4) for humidity sensor
+ //  
+ //  
  unsigned int val; 
  PINSEL0 |=  0x0F300000;   /* Select the P0_13 AD1.4 for ADC function */ 
                                              /* Select the P0_12 AD1.3 for ADC function */ 
@@ -84,10 +103,46 @@ unsigned int adc(int no,int ch)
     return val; 
 }
 
+void doorOpening()
+{
+	//P0.12 for IR sensor
+	unsigned int digitalValue = !(IO1PIN & (1<<12)); //read value of port P0.12
+	if(digitalValue==1)
+	{
+		beep(); //buzzer 
+		if(!DOOR_OPEN) //switch to open the door. If it is pressed the stepper motor attached to the door opens the door.
+		{
+			unsigned int  no_of_steps_clk = 50, no_of_steps_aclk = 50; 
+		 IO0DIR |= 1U << 31 | 0x00F00000 | 1U << 30; // to set P0.20 to P0.23 as o/ps  
+		 
+		 
+		do{ 
+		IO0CLR = 0X00F00000;IO0SET = 0X00100000;delay_ms(10);if(--no_of_steps_clk == 0) break; 
+		IO0CLR = 0X00F00000;IO0SET = 0X00200000;delay_ms(10);if(--no_of_steps_clk == 0) break; 
+		IO0CLR = 0X00F00000;IO0SET = 0X00400000;delay_ms(10);if(--no_of_steps_clk == 0) break; 
+		IO0CLR = 0X00F00000;IO0SET = 0X00800000;delay_ms(10);if(--no_of_steps_clk == 0) break; 
+		}while(1); 
+		while((digitalValue = (IO1PIN & (1<<12)))==1);
+		do{ 
+		IO0CLR = 0X00F00000;IO0SET = 0X00800000;delay_ms(10);if(--no_of_steps_aclk == 0) break; 
+		IO0CLR = 0X00F00000;IO0SET = 0X00400000;delay_ms(10);if(--no_of_steps_aclk == 0) break; 
+		IO0CLR = 0X00F00000;IO0SET = 0X00200000;delay_ms(10);if(--no_of_steps_aclk == 0) break; 
+		IO0CLR = 0X00F00000;IO0SET = 0X00100000;delay_ms(10);if(--no_of_steps_aclk == 0) break; 
+		}while(1); 
+		IO0CLR = 0X00F00000; 
+		 
+
+		}
+	}
+}
+
+
 int main()
 {
+	SystemInit( ); 
 	do
 	{
+		doorOpening();
 		plantWatering();
 	}while(1);
 	return 0;
